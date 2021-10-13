7e5f0b9179b487d3d18b1898dc6fab4f55a93fbb
mikegomdi <mikegomez1493@gmail.com>
2021-10-13 07:55:20 -0500

    TS2

--- trianArreglos.py
+++ trianArreglos.py
@@ -13,5 +13,16 @@ Created on Wed Oct 13 07:41:52 2021
 #2. Declarar la matriz y asignar el tamaño
 #3. Guardar en la matriz el valor de los lados de cada triangulo
 #4. Calcular el tipo de triangulo
+
 # Captura el numero de triangulos
+import numpy as np
 nTriangulos = int(input("Cuantos triángulos desea almacenar?"))
+nLados = 3
+
+matriz = np.zeros([nTriangulos,nLados])
+
+for i in range(0,nTriangulos):
+    for j in range(0,nLados):
+        print("Triangulo" , i+1, "lado", j+1)
+        matriz[i,j] = int(input())
+        
