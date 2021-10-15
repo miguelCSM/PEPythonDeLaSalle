2df38bb17cf0215e78b01428fa2fd6d251a8e61b
mikegomdi <mikegomez1493@gmail.com>
2021-10-15 09:11:57 -0500

    Triangulos tipos

--- trianArreglos.py
+++ trianArreglos.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 13 07:41:52 2021
-
-@author: migue
-"""
 
 # Ejercicio En un arreglo guardarán las medidas de los lados de
 # n-triangulos indicados por el usuario. Requieren capturar el tamaño
@@ -17,24 +12,51 @@ Created on Wed Oct 13 07:41:52 2021
 # Captura el numero de triangulos
 import numpy as np
 nTriangulos = int(input("Cuantos triángulos desea almacenar?"))
+metodo = int(input("Por cual método gusta clasificar 1. Lados 2. Ángulos"))
+
 nLados = 3
 tipos = []
-
 matriz = np.zeros([nTriangulos,nLados])
 
-for i in range(0,nTriangulos):
-    for j in range(0,nLados):
-        print("Triangulo" , i+1, "lado", j+1)
-        matriz[i,j] = int(input())
-
-for i in range(0,nTriangulos):
-    if(matriz[i,0] == matriz[i,1] and matriz[i,0] == matriz[i,2]):
-        tipos.append("Equilatero")
-    elif (matriz[i,0]==matriz[i,1] or matriz[i,1]==matriz[i,2] or matriz[i,0]== matriz[i,2]):
-        tipos.append("Isosceles")
-    else:
-        tipos.append("Escaleno")
+if (metodo == 1):
+    # Almacenan los lados 
+    for i in range(0,nTriangulos):
+        for j in range(0,nLados):
+            print("Triangulo" , i+1, "lado", j+1)
+            matriz[i,j] = int(input())
+    
+    for i in range(0,nTriangulos):
+        if(matriz[i,0] == matriz[i,1] and matriz[i,0] == matriz[i,2]):
+            tipos.append("Equilatero")
+        elif (matriz[i,0]==matriz[i,1] or matriz[i,1]==matriz[i,2] or matriz[i,0]== matriz[i,2]):
+            tipos.append("Isosceles")
+        else:
+            tipos.append("Escaleno")
         
+elif (metodo == 2):
+    for i in range(0,nTriangulos):
+        for j in range(0,nLados):
+            print("Triangulo" , i+1, "Ángulo", j+1)
+            matriz[i,j] = int(input())
+        suma = matriz[i,:].sum()
+        
+        if (suma != 180):
+            for j in range(0,nLados):
+                print("Tienes que repetir la captura del tringulo")
+                print("Triangulo" , i+1, "Ángulo", j+1)
+                matriz[i,j] = int(input())
+            
+            
+    for i in range(0,nTriangulos):
+        if(matriz[i,0]==90 or matriz[i,1]==90 or matriz[i,2]==90):
+            tipos.append("Rectángulo")
+        elif (matriz[i,0]<90 and matriz[i,1]<90 and matriz[i,2]<90):
+            tipos.append("Acutángulo")
+        else:
+            if (matriz[i,0]>90 and matriz[i,0]<180 or matriz[i,1]>90 and matriz[i,1]<180 or matriz[i,2]>90 and matriz[i,2]<180):
+                tipos.append("Obtusángulo")
+                
+                
 
         
         
