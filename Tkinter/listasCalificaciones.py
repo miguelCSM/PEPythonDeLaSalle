c7b88a8eaa31bf1392e1884e5a79798cb7f75392
mikegomdi <mikegomez1493@gmail.com>
2021-11-10 07:35:13 -0600

    control de inventarios

--- Tkinter/listasCalificaciones.py
+++ Tkinter/listasCalificaciones.py
@@ -19,12 +19,35 @@ Lcal1 = []
 Lcal2 = []
 Lcal3 = []
 
-def add():
-    Lcal1.append(cal1.get())
-    Lcal2.append(cal2.get())
-    Lcal3.append(cal3.get())
-    print(Lcal1,Lcal2,Lcal3)
 
+
+def add():
+    Lcal1.append(float(cal1.get()))
+    Lcal2.append(float(cal2.get()))
+    Lcal3.append(float(cal3.get()))
+    # Limpiar entradas
+    cal1.delete(0,END)
+    # cal1.insert(0,"0")
+    cal2.delete(0,END)
+    cal3.delete(0,END)
+    
+    #print(Lcal1,Lcal2,Lcal3)
+
+def promedio():
+    cols = 3
+    rows = len(Lcal1)
+    
+    # convertir lista en arreglo
+    arr1 = np.asarray(Lcal1)
+    arr2 = np.asarray(Lcal2)
+    arr3 = np.asarray(Lcal3)
+    
+    #creamos una matriz de calificaciones
+    matriz = np.array([arr1,arr2,arr3])  
+    prom1 = matriz[0,:].mean()        
+    
+    print(matriz, type(matriz), prom1)
+    
 lbl1 = Label(window, text= "Calif. 1")
 lbl1.place(x=0, y=0)
 
@@ -43,10 +66,22 @@ lbl3.place(x=0, y=80)
 cal3 = Entry(window)
 cal3.place(x=0,y=100)
 
+promedios = Label(window, text= "Promedios")
+promedios.place(x=280,y=20)
+
+p1lbl =  Label(window, text = "Periodo 1")
+p1lbl.place(x= 150,y=40)
+
+p2lbl =  Label(window, text = "Periodo 2")
+p2lbl.place(x= 280,y=40)
+
+p3lbl =  Label(window, text = "Periodo 3")
+p3lbl.place(x= 430,y=40)
+
 btnAdd = Button(window, text= "Agregar", command = add)
 btnAdd.place(x = 0, y = 130)
 
-btnCalc = Button(window, text= "Promedios", command = "")
+btnCalc = Button(window, text= "Promedios", command = promedio)
 btnCalc.place(x = 80, y = 130)
 
 window.geometry("500x250")
