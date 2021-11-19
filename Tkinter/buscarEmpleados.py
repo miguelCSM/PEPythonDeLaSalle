6b367237553cdf313ab01ad2913bcce4631b332f
mikegomdi <mikegomez1493@gmail.com>
2021-11-19 12:46:34 -0600

    Búsqueda empleados

--- Tkinter/buscarEmpleados.py
+++ Tkinter/buscarEmpleados.py
@@ -5,8 +5,28 @@ Created on Fri Nov 19 07:30:42 2021
 @author: migue
 """
 
-MiLista = ["Miguel", "Krauus", "Maribel", "Krauus", "Miguel", "Krauus"]
+from tkinter import *
 
-contar = {i:MiLista.count(i) for i in MiLista}
+MiLista = ["Miguel", "Krauus", "Maribel"]
+edadLista = [21,18,17]
 
-print(contar)
+window = Tk()
+
+window.title("Control de inventarios")
+
+window.geometry("600x300")
+
+xP = 0
+yP = 0
+
+for i in range(len(MiLista)):
+    nombre = MiLista[i]
+    lbl1 = Label(window, text= nombre)
+    lbl1.place(x=xP, y=yP)
+    edad = edadLista[i]
+    lbl2 = Label(window, text= edad)
+    lbl2.place(x=100, y=yP)
+    yP +=50
+    
+
+window.mainloop()
